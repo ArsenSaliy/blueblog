b1ade882331c2be0ce5f72fa62c4db0e2a94526d
arsensaliy <arsen.saliy@gmail.com>
2017-03-16 15:08:32 +0200

    third_commit

--- src/blueblog/settings.py
+++ src/blueblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGIN_URL = '/login/'
